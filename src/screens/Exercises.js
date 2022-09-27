059ec47236c2e1417781b8bf96506158267de726
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 13:25:13 +0100

    added additional code structure

--- src/screens/Exercises.js
+++ src/screens/Exercises.js
@@ -1,5 +1,4 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
 import ExerciseList from '../components/exercises/ExerciseList';
 
 export default function Exercises() {
