c1541f7a7fd0a6fff1afd7550ccfd751025877e9
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 11:33:35 +0100

    linted files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,7 @@ const dotenvPlugin = require('cypress-dotenv');
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
