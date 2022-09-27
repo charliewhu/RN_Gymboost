e0c282bd82d16c7ae6fe8a472b59da7218daf2eb
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 10:27:14 +0100

    added cypress and eslint config

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const {defineConfig} = require('cypress');
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:19006',
+  },
+});
