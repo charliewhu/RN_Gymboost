ec3581b7b80462092abfea5d143a4dcc4a24c657
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 11:56:45 +0100

    added additonal linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  plugins: ['import', 'cypress'],
+  plugins: ['import', 'cypress', 'unused-imports'],
   env: {
     'cypress/globals': true,
   },
   rules: {
+    'unused-imports/no-unused-imports': 'error',
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     'no-duplicate-imports': 'error',
     quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
@@ -15,6 +16,17 @@ module.exports = {
     'sort-imports': [
       'warn',
       {ignoreDeclarationSort: true, ignoreMemberSort: false},
-    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+    ],
+    // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
---
+++ src/Navigation.js
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export default function Navigation() {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  );
+}
