3f862eb7babccb7493b6dda4e37dd14466dfbae1
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 12:34:08 +0100

    added ExerciseList to Exercises Screen

--- src/screens/Exercises.js
+++ src/screens/Exercises.js
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import ExerciseList from '../components/exercises/ExerciseList';
 
 export default function Exercises() {
   return (
     <View>
-      <Text>Exercises</Text>
+      <ExerciseList />
     </View>
   );
 }
