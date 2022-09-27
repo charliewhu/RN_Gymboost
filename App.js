b3793e285171f3afa41b5d5cc8e729ef051bb6cb
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 21:03:14 +0100

    Exercise test failing

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import {Provider} from 'react-redux';
+
+import store from './src/redux/store';
 import Navigation from './src/Navigation';
 
 export default function App() {
   return (
-    <PaperProvider>
-      <SafeAreaProvider>
-        <StatusBar style="auto" />
-        <Navigation />
-      </SafeAreaProvider>
-    </PaperProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <StatusBar style="auto" />
+          <Navigation />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </Provider>
   );
 }
