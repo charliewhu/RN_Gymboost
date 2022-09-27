01cc2d24911e5189bd6e7be456154caa951a11d0
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 12:30:38 +0100

    added stack navigator

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,9 +1,30 @@
 import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Exercises from './screens/Exercises';
+
+const linking = {
+  config: {
+    screens: {
+      Exercises: {
+        path: '/exercises',
+        initialRouteName: 'Exercises',
+        screens: {
+          Exercises: '',
+        },
+      },
+    },
+  },
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Exercises" component={Exercises} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
