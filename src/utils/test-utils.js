dfeaddc54ec0016e3ea08428652ab0636af0c21c
Charlie <charlieavery123@hotmail.co.uk>
2022-09-27 18:13:13 +0100

    added redux test util

---
+++ src/utils/test-utils.js
@@ -0,0 +1,10 @@
+import {render} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import store from '../redux/store';
+
+export function renderWithProviders(ui, {...renderOptions} = {}) {
+  function Wrapper({children}) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return {store, ...render(ui, {wrapper: Wrapper, ...renderOptions})};
+}
