5933173f0856439b9192661c56fccfcace665998
Charlie <charlieavery123@hotmail.co.uk>
2022-09-28 14:25:16 +0100

    refactored test to use fixture

--- cypress/e2e/exercises/list.cy.js
+++ cypress/e2e/exercises/list.cy.js
@@ -1,21 +1,23 @@
+import exercises from '../../fixtures/exercises.json';
+
 const API_URL = Cypress.env('API_URL');
 
-describe('Exercise List', () => {
+describe('Exercises page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `${API_URL}/exercises/`, {
+      fixture: 'exercises.json',
+    }).as('getExercises');
+
+    cy.fixture('exercises.json').as('exercises');
+  });
+
   it('shows exercises from the server', () => {
-    const exercise1 = 'Ex1';
-    const exercise2 = 'Ex2';
-
-    cy.intercept('GET', `${API_URL}/exercises/`, [
-      {id: 1, name: exercise1},
-      {id: 2, name: exercise2},
-    ]).as('getExercises');
-
     cy.visit('/exercises/');
     cy.wait('@getExercises');
 
     cy.findByTestId('exercise_list').should('be.visible');
 
-    cy.contains(exercise1);
-    cy.contains(exercise2);
+    cy.contains(exercises[0].name);
+    cy.contains(exercises[0].name);
   });
 });
