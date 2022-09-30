543823c35c222e7a3dc52b37c086d034f5143693
Charlie <charlieavery123@hotmail.co.uk>
2022-09-30 11:49:57 +0100

    wrote test to navigate to WorkoutExercises

--- cypress/e2e/workouts/Workouts.cy.js
+++ cypress/e2e/workouts/Workouts.cy.js
@@ -8,6 +8,10 @@ describe('Workouts screen', () => {
       fixture: 'workouts.json',
     }).as('getWorkouts');
 
+    cy.intercept('GET', `${API_URL}/workoutexercises/`, {
+      fixture: 'workoutexercises.json',
+    }).as('getWorkoutExercises');
+
     cy.fixture('workouts.json').as('workouts');
 
     cy.visit('/workouts/');
@@ -19,5 +23,13 @@ describe('Workouts screen', () => {
 
     cy.contains(new Date(Date.parse(workouts[0].created_on)).toUTCString());
     cy.contains(new Date(Date.parse(workouts[1].created_on)).toUTCString());
+  });
+
+  it('navigates to a Workout', () => {
+    cy.findAllByTestId('workout_list_item').first().click();
+
+    cy.wait('@getWorkoutExercises');
+
+    cy.findByTestId('workout_exercise_list');
   });
 });
