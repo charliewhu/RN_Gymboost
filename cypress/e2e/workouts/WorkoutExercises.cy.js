d76ba7b95f90dd44bc12e8e1938413afe54dab7d
Charlie <charlieavery123@hotmail.co.uk>
2022-09-30 11:04:39 +0100

    added workoutexercises fixture and started test

---
+++ cypress/e2e/workouts/WorkoutExercises.cy.js
@@ -0,0 +1,16 @@
+import workoutexercises from '../../fixtures//workoutexercises.json';
+
+const API_URL = Cypress.env('API_URL');
+
+describe('WorkoutExercises screen', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `${API_URL}/workoutexercises/`, {
+      fixture: 'workoutexercises.json',
+    }).as('getWorkoutExercises');
+
+    cy.fixture('workoutexercises.json').as('workoutexercises');
+
+    cy.visit('/workoutexercises/');
+    cy.wait('@getWorkoutExercises');
+  });
+});
