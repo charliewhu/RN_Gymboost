f9076265754583891634186b3730ce114573fc17
Charlie <charlieavery123@hotmail.co.uk>
2022-10-01 15:40:29 +0100

    added nav button from WorkoutExercise

--- src/screens/workouts/WorkoutExercises.js
+++ src/screens/workouts/WorkoutExercises.js
@@ -1,5 +1,22 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {useLayoutEffect} from 'react';
+import {TouchableOpacity} from 'react-native';
+
 import WorkoutExerciseList from '../../components/workouts/WorkoutExerciseList';
 
-export default function WorkoutExercises({route}) {
+export default function WorkoutExercises({navigation, route}) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          testID="addExerciseBtn"
+          onPress={() => console.log('addExerciseBtn pressed')}
+        >
+          <Ionicons name="add" size={30} />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
   return <WorkoutExerciseList route={route} />;
 }
