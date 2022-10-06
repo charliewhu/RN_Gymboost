69bafa6e860845d824879db7efcb461069e892f7
Charlie <charlieavery123@hotmail.co.uk>
2022-10-06 10:48:55 +0100

    added addExerciseBtn

--- src/screens/routines/RoutineExercises.js
+++ src/screens/routines/RoutineExercises.js
@@ -1,5 +1,24 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {useLayoutEffect} from 'react';
+import {TouchableOpacity} from 'react-native';
+
 import RoutineExerciseList from '../../components/routines/RoutineExerciseList';
 
-export default function RoutineExercises({route}) {
+export default function RoutineExercises({navigation, route}) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          testID="addExerciseBtn"
+          onPress={() =>
+            navigation.navigate('ExerciseScreen', {id: route.params.id})
+          }
+        >
+          <Ionicons name="add" size={30} />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
   return <RoutineExerciseList route={route} />;
 }
