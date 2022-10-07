5155372b4e12d4f67d34fe0ea17f834ae62ff86a
Charlie <charlieavery123@hotmail.co.uk>
2022-10-07 11:25:40 +0100

    added deleteRoutineBtn

--- src/components/routines/RoutineListItem.js
+++ src/components/routines/RoutineListItem.js
@@ -1,6 +1,16 @@
 import {ListItem} from '@rneui/themed';
+import {useDispatch} from 'react-redux';
+import {deleteWorkout} from '../../redux/routine/routineSlice';
+
+import IconButton from '../utils/IconButton';
 
 export default function RoutineListItem({navigation, item}) {
+  const dispatch = useDispatch();
+
+  const handleDelete = id => {
+    dispatch(deleteWorkout(id));
+  };
+
   return (
     <ListItem
       testID="routine_list_item"
@@ -13,6 +23,11 @@ export default function RoutineListItem({navigation, item}) {
       <ListItem.Content>
         <ListItem.Title>{item.name}</ListItem.Title>
       </ListItem.Content>
+      <IconButton
+        testID="deleteRoutineBtn"
+        icon="remove-circle-outline"
+        onPress={() => handleDelete(item.id)}
+      />
     </ListItem>
   );
 }
