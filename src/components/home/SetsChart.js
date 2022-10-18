2fad3bef051bd8ecdcf55929d64fc773ecb45d87
Charlie <charlieavery123@hotmail.co.uk>
2022-10-18 15:42:42 +0100

    chart rendering

--- src/components/home/SetsChart.js
+++ src/components/home/SetsChart.js
@@ -1,8 +1,43 @@
-import {Text, View} from 'react-native';
-export default function SetsChart() {
+import {LineChart} from 'react-native-chart-kit';
+
+export default function SetsChart({workouts, chartConfig, screenWidth}) {
+  const workoutsByDate = workouts.map(item => ({
+    date: new Date(Date.parse(item.created_on)).toDateString(),
+    total_sets: item.total_sets,
+  }));
+
+  var setsByDate = workoutsByDate.reduce((acc, item) => {
+    let existItem = acc.find(({date}) => item.date === date);
+    if (existItem) {
+      existItem.total_sets += item.total_sets;
+    } else {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
+
+  const data = {
+    labels: setsByDate.map(item => item.date),
+    datasets: [
+      {
+        data: setsByDate.map(item => item.total_sets),
+      },
+    ],
+  };
+
+  console.log('sets:', setsByDate);
+
   return (
-    <View>
-      <Text>SetsChart</Text>
-    </View>
+    <LineChart
+      data={data}
+      width={screenWidth}
+      height={260}
+      verticalLabelRotation={30}
+      chartConfig={chartConfig}
+      //fromZero={true}
+      yAxisInterval={2}
+      xLabelsOffset={5}
+      bezier
+    />
   );
 }
