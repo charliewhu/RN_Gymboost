7d9a92c9994d7c71edc5a91b257de1626542b5b3
Charlie <charlieavery123@hotmail.co.uk>
2022-10-18 14:11:02 +0100

    amended to be contained in wrapper

--- src/components/home/ContribChart.js
+++ src/components/home/ContribChart.js
@@ -1,45 +1,22 @@
-import {ListItem, Text} from '@rneui/themed';
-import {View} from 'react-native';
 import {ContributionGraph} from 'react-native-chart-kit';
 
-export default function ContribChart({
-  workouts,
-  theme,
-  styles,
-  chartConfig,
-  screenWidth,
-}) {
+export default function ContribChart({workouts, chartConfig, screenWidth}) {
   const workoutContribs = workouts.map(item => ({
     date: new Date(Date.parse(item.created_on)),
     count: 1,
   }));
 
   return (
-    <>
-      <Text
-        style={{
-          ...styles.chartTitle,
-          backgroundColor: theme.colors.background,
-        }}
-      >
-        Days Worked Out
-      </Text>
-
-      <ListItem>
-        <View style={styles.chartView}>
-          <ContributionGraph
-            testID="workoutContribGraph"
-            values={workoutContribs}
-            numDays={92}
-            width={Math.min(screenWidth - 20, 400)}
-            height={220}
-            chartConfig={chartConfig}
-            //showMonthLabels={false}
-            //onDayPress={item => console.log(item)}
-            gutterSize={3}
-          />
-        </View>
-      </ListItem>
-    </>
+    <ContributionGraph
+      testID="workoutContribGraph"
+      values={workoutContribs}
+      numDays={92}
+      width={Math.min(screenWidth - 20, 400)}
+      height={220}
+      chartConfig={chartConfig}
+      //showMonthLabels={false}
+      //onDayPress={item => console.log(item)}
+      gutterSize={3}
+    />
   );
 }
