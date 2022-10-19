cb78ed849e83e04af5f7717b45015ab584d2124e
Charlie <charlieavery123@hotmail.co.uk>
2022-10-19 16:00:33 +0100

    added custom selector and fetch

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,14 +3,17 @@ import {useEffect} from 'react';
 import {ScrollView, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {getWorkouts} from '../redux/workout/workoutSlice';
+import {getWorkoutExercises} from '../redux/workoutExercise/workoutExerciseSlice';
+import {
+  getSetCount,
+  getWorkoutExerciseSets,
+} from '../redux/workoutExerciseSet/workoutExerciseSetSlice';
 
 export default function Home() {
   const dispatch = useDispatch();
   const today = new Date();
   const workouts = useSelector(state => state.workout.workouts);
-  const totalSets = useSelector(state =>
-    state.workout.workouts.reduce((prev, curr) => prev + curr.total_sets, 0),
-  );
+  const totalSets = useSelector(state => getSetCount(state));
   const totalWeekSets = useSelector(state =>
     state.workout.workouts
       .filter(
@@ -23,6 +26,8 @@ export default function Home() {
 
   useEffect(() => {
     dispatch(getWorkouts());
+    dispatch(getWorkoutExercises());
+    dispatch(getWorkoutExerciseSets());
   }, [dispatch]);
 
   return (
