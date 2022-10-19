81d0b430f3f9ee088228e60aeba3b4a7aac34d68
Charlie <charlieavery123@hotmail.co.uk>
2022-10-19 16:13:52 +0100

    unit test for getTotalWeekSets

--- src/redux/workoutExerciseSet/workoutExerciseSetSlice.test.js
+++ src/redux/workoutExerciseSet/workoutExerciseSetSlice.test.js
@@ -35,4 +35,67 @@ describe('selectors', () => {
       expect(result).toEqual(2);
     });
   });
+
+  describe('getTotalWeekSets', () => {
+    it('returns 0 with no items', () => {
+      const state = {
+        workout: {
+          workouts: [
+            {id: 1, created_on: '2022-09-02T10:49:07.982317Z'},
+            {id: 2, created_on: '2022-09-03T10:49:07.982317Z'},
+          ],
+        },
+        workoutExercise: {
+          workoutExercises: [
+            {
+              id: 1,
+              workout: 1,
+            },
+          ],
+        },
+        workoutExerciseSet: {
+          workoutExerciseSets: [
+            {
+              id: 1,
+              workout_exercise: 1,
+            },
+          ],
+        },
+      };
+
+      const result = getTotalWeekSets(state);
+      expect(result).toEqual(0);
+    });
+
+    it('returns 1 with 1 items', () => {
+      const state = {
+        workout: {
+          workouts: [
+            {id: 1, created_on: new Date().toISOString()},
+            {id: 2, created_on: '2022-09-03T10:49:07.982317Z'},
+            {id: 3, created_on: '2022-09-03T10:49:07.982317Z'},
+          ],
+        },
+        workoutExercise: {
+          workoutExercises: [
+            {
+              id: 1,
+              workout: 1,
+            },
+          ],
+        },
+        workoutExerciseSet: {
+          workoutExerciseSets: [
+            {
+              id: 1,
+              workout_exercise: 1,
+            },
+          ],
+        },
+      };
+
+      const result = getTotalWeekSets(state);
+      expect(result).toEqual(1);
+    });
+  });
 });
