ac03b81beb972921ceb04e204d330e69d7502fa2
Charlie <charlieavery123@hotmail.co.uk>
2022-10-20 14:25:11 +0100

    ActionButton can open FormModal

--- src/components/workoutExerciseSets/ActionButton.js
+++ src/components/workoutExerciseSets/ActionButton.js
@@ -1,13 +1,18 @@
 import {SpeedDial} from '@rneui/themed';
 import hexToRgba from 'hex-to-rgba';
 
-export default function ActionButton({theme, fabOpen, setFabOpen}) {
+export default function ActionButton({
+  theme,
+  fabOpen,
+  setFabOpen,
+  setModalIsVisible,
+}) {
   return (
     <SpeedDial
       testID="actionBtn"
       isOpen={fabOpen}
-      icon={{name: 'edit'}}
-      openIcon={{name: 'close'}}
+      icon={{name: 'edit', color: theme.colors.white}}
+      openIcon={{name: 'close', color: theme.colors.white}}
       onOpen={() => setFabOpen(!fabOpen)}
       onClose={() => setFabOpen(!fabOpen)}
       color={theme.colors.primary}
@@ -15,11 +20,12 @@ export default function ActionButton({theme, fabOpen, setFabOpen}) {
     >
       <SpeedDial.Action
         testID="addSetBtn"
-        icon={{name: 'add'}}
+        icon={{name: 'add', color: theme.colors.white}}
         color={theme.colors.primary}
         title="Add Set"
         onPress={() => {
           setFabOpen(false);
+          setModalIsVisible(true);
         }}
       />
     </SpeedDial>
