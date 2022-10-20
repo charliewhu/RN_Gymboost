b77aaaaf46b4ab5ec3f29861afc3c2e89a08f978
Charlie <charlieavery123@hotmail.co.uk>
2022-10-20 14:25:40 +0100

    added wireframe FormModal component

--- src/components/workoutExerciseSets/FormModal.js
+++ src/components/workoutExerciseSets/FormModal.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import {BottomSheet, Text} from '@rneui/themed';
+import {BottomSheet, ListItem} from '@rneui/themed';
+import {StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function FormModal({modalIsVisible}) {
+export default function FormModal({modalIsVisible, setModalIsVisible}) {
   return (
-    <BottomSheet modalProps={{}} isVisible={modalIsVisible}>
-      <Text>Modal</Text>
-    </BottomSheet>
+    <SafeAreaProvider>
+      <BottomSheet
+        modalProps={{}}
+        isVisible={modalIsVisible}
+        onBackdropPress={() => setModalIsVisible(false)}
+      >
+        <ListItem bottomDivider={false}>
+          <ListItem.Content style={styles.listItem}>
+            <ListItem.Title>Modal Item 1</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+        <ListItem bottomDivider={false}>
+          <ListItem.Content style={styles.listItem}>
+            <ListItem.Title>Modal Item 2</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      </BottomSheet>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  listItem: {
+    height: 100,
+  },
+});
